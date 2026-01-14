5174897ea2a83a79c59558b4138b5a1659cd8b05
bestinh4 <diiogo49@gmail.com>
2026-01-14 20:47:44 -0300

    Initialize Firebase app and Firestore database

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAK-Mj7fDwCUh9aer3z8swN7hUNIi2FK4E",
+  authDomain: "ousadia-alegria-3269f.firebaseapp.com",
+  projectId: "ousadia-alegria-3269f",
+  storageBucket: "ousadia-alegria-3269f.firebasestorage.app",
+  messagingSenderId: "695364420342",
+  appId: "1:695364420342:web:aa130dfa6e019a271b22d7"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.db = db;
 
