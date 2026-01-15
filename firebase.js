0bf94d6c060b0f64e66c193a38200905db2f34de
bestinh4 <diiogo49@gmail.com>
2026-01-15 19:02:52 -0300

    Refactor Firebase initialization and service access
    Updated Firebase initialization and service exports.

--- firebase.js
+++ firebase.js
@@ -1,17 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
-
+// Inicialização Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAK-Mj7fDwCUh9aer3z8swN7hUNIi2FK4E",
   authDomain: "ousadia-alegria-3269f.firebaseapp.com",
-  projectId: "ousadia-alegria-3269f",
-  storageBucket: "ousadia-alegria-3269f.firebasestorage.app",
-  messagingSenderId: "695364420342",
-  appId: "1:695364420342:web:aa130dfa6e019a271b22d7"
+  projectId: "ousadia-alegria-3269f"
 };
 
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+// Serviços globais
+window.auth = firebase.auth();
+window.db = firebase.firestore();
