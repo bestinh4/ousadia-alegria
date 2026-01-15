13a2f52b2eb290046744de348c3b23c38106daf5
bestinh4 <diiogo49@gmail.com>
2026-01-15 19:15:24 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,15 @@
-// Inicialização Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAK-Mj7fDwCUh9aer3z8swN7hUNIi2FK4E",
   authDomain: "ousadia-alegria-3269f.firebaseapp.com",
-  projectId: "ousadia-alegria-3269f"
+  projectId: "ousadia-alegria-3269f",
+  storageBucket: "ousadia-alegria-3269f.firebasestorage.app",
+  messagingSenderId: "695364420342",
+  appId: "1:695364420342:web:aa130dfa6e019a271b22d7"
 };
 
+// Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
 
 // Serviços globais
-window.auth = firebase.auth();
-window.db = firebase.firestore();
+const auth = firebase.auth();
+const db = firebase.firestore();
